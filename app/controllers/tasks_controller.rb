12c35d2772211d095e10a62d4d1177b996bfe73e
April Yeats <yeats.april@gmail.com>
2020-09-22 14:07:27 +0200

    Completed challemge: rails task manager - still ungly as hell but have refactored some code

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params['id'])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -18,13 +18,9 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
-  def edit
-    @task = Task.find(params['id'])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params['id'])
-
     if @task.update(task_params)
       redirect_to task_path(@task)
     else
@@ -33,8 +29,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
-
     @task.destroy
     if @task.destroy
       redirect_to tasks_path
@@ -48,4 +42,9 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
+
+  def set_task
+    @task = Task.find(params['id'])
+  end
+
 end
